9472d1eb6ec27e8953c46006e8450814efcd751f
StrugariStefan <mihaistrugari@yahoo.com>
2018-03-20 13:29:29 +0200

    resp form + chart

--- chart.js
+++ chart.js
@@ -1,33 +1,46 @@
 var chart;
 
-function makeChart() {
+setInterval( function() { 
+	addPoint( 
+		{ y : Math.random() * ( document.getElementById("inter").innerHTML.split("-")[1] 
+		- document.getElementById("inter").innerHTML.split("-")[0] ) 
+		+ parseFloat(document.getElementById("inter").innerHTML.split("-")[0]) }
+	); 
+}, 
+document.getElementById("time").innerHTML * 1000 );
+
+function makeChart( values ) {
 	chart = new CanvasJS.Chart("chartContainer", { 
         zoomEnabled: true,
 		title: {
 			text: "EURO->RON "
-        },
+		},
+		axisX: {
+			minimum: 0,
+			title : "Time units"
+		},
         axisY: {
-            includeZero: false
+			minimum: document.getElementById("inter").innerHTML.split("-")[0],
+			maximum: document.getElementById("inter").innerHTML.split("-")[1],
+			includeZero: false,
+			title: "Value"
         },
 		data: [
 		{
 			type: "spline",
-			dataPoints: [
-				{ y : 1 },
-				{ y : 2 },
-				{ y : 3 },
-				{ y : 4 }
-			]
+			dataPoints: values
 		}
 		]
 	});
+	document.getElementById("currentValue").innerHTML = chart.options.data[0].dataPoints[chart.options.data[0].dataPoints.length-1].y;
     chart.render();
 }
 
-function addPoint() {
+function addPoint( newValue ) {
 
 	var length = chart.options.data[0].dataPoints.length;
-	chart.options.data[0].dataPoints.push({ y: 25 - Math.random() * 10});
+	chart.options.data[0].dataPoints.push( newValue );
+	document.getElementById("currentValue").innerHTML = chart.options.data[0].dataPoints[length].y;
 	chart.render();
 
 }
@@ -38,4 +51,10 @@ function updatePoint() {
 	chart.options.data[0].dataPoints[length-1].y = 15 - Math.random() * 10;
 	chart.render();
 
-}    
+}
+
+/*function changeBaseCurrency() {
+	makeChart([]);
+	document.getElementById("Current").innerHTML = this.innerHTML;
+}*/
+
